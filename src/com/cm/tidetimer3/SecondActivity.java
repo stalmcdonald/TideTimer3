9c8750165296adbf3a73599acc9601e7db97d79b
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-08-18 11:04:39 -0700

    Implicit Intent Implemented
    browser button goes to http://www.wunderground.com/ for forecast
    information.  Should add some text explaining more.

--- src/com/cm/tidetimer3/SecondActivity.java
+++ src/com/cm/tidetimer3/SecondActivity.java
@@ -6,6 +6,53 @@
  */
 package com.cm.tidetimer3;
 
-public class SecondActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
+public class SecondActivity extends Activity implements OnClickListener {
+	Button goTide;
+	Button browse;
+	EditText cityEntered;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tide);
+		cityEntered = (EditText)findViewById(R.id.cityText);
+		goTide = (Button)findViewById(R.id.tideButton);
+		
+		//set listener for browser button
+		browse = (Button)findViewById(R.id.webButton);
+		
+		goTide.setOnClickListener(this);
+		
+		//Implicit Intent
+		browse.setOnClickListener(new OnClickListener() {
+		@Override
+		public void onClick(View interwebs) {
+			// sends user to site that shows more information on forecast
+			Uri uriUrl = Uri.parse("http://www.wunderground.com/");
+			
+			Intent browse_intent = new Intent(Intent.ACTION_VIEW, uriUrl);
+			
+			startActivity(browse_intent);
+			
+		}
+	});
+
+	
+
+}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 }
